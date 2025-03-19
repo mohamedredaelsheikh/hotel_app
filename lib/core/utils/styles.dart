0c9962f00905e4986bc8ccabb6934eeb1b65f390
mohamedredaelsheikh <moh.reda198@gmail.com>
2025-03-19 06:26:03 +0200

    add styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_app/core/utils/constants.dart';
+
+abstract class Styles {
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w800,
+    fontFamily: kRibeyeMarrow,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
