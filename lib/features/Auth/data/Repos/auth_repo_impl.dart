471b38409d477860ef2dfd5d0c677e3978a87be5
Mohamedredaelsheikh <moh.reda198@gmail.com>
2025-04-24 04:29:35 +0200

    add auth repo  impl sign up to data layer

---
+++ lib/features/Auth/data/Repos/auth_repo_impl.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:hotel_app/features/Auth/Domain/Repos/auth_repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  @override
+  Future<Either> signUp() {
+    throw UnimplementedError();
+  }
+}
